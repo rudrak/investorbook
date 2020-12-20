d7c5d331ff5e1231a7e242b8912c219f6c533422
Piu Sharma <piu.sharma@nutanix.com>
2020-12-20 21:38:04 +0530

    Adds header with pseudo logo

---
+++ src/components/AppHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './appHeader.css';
+
+export default () => {
+  return (
+    <div className="header">
+        <span className="blueLogo">Investor</span>Book
+    </div>
+  );
+}
